e21c7984941776ed9edd439aa1fdd80a29a0eb1c
jay <dj.min43@gmail.com>
2021-11-04 07:01:34 +0900

    add api endpoints

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react'
 import type { NextPage } from 'next'
+import axios from 'axios'
 
 const Body: NextPage = () => {
+
+  const [mainQuote, setMainQuote] = useState<string>('')
+
+  const getKanyeQuote = async () => {
+    const result: string = await axios.get('https://api.kanye.rest')
+    setMainQuote(result)
+    console.log(mainQuote)
+  }
+
+  useEffect(() => {
+    getKanyeQuote()
+  })
+
   return (
     <div>
       body
