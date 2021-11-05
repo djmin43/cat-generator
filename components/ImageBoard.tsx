66e4e5a023f23441547c3cfc821710104fd90ceb
jay <dj.min43@gmail.com>
2021-11-05 14:24:03 +0900

    add image container

--- components/ImageBoard.tsx
+++ components/ImageBoard.tsx
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
+import { css } from '@emotion/css'
+
+
 
 const ImageBoard = () => {
 
   const [catImage, setCatImage] = useState('')
 
   return (
-    <div>
-      <Image src="https://cataas.com/cat" alt="cat" />
+    <div className={imageContainerStyle}>
+      <Image src="https://cataas.com/cat" alt="cat" layout="fill" objectFit="contain"/>
     </div>
   )
 }
 
 
-const getCatImage = async (text: string): string => {
+// const getCatImage = async (text: string): void => {
   
-}
+// }
+
+
+const imageContainerStyle = css`
+  width: 15vw;
+  height: 15vw;
+  border: 1px solid rgba(0, 0, 0, 0.12);
+`
 
 export default ImageBoard
