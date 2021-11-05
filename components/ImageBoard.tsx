54ec709cf04b55ec1065b160cff729d22f14d6cb
jay <dj.min43@gmail.com>
2021-11-05 14:58:47 +0900

    add input logic

--- components/ImageBoard.tsx
+++ components/ImageBoard.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import Image from 'next/image'
 import { css } from '@emotion/css'
 
 interface CatImageProps {
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { css } from '@emotion/css'
 
 interface InputProps {
   getCatImage: (text: string) => Promise<void>
 }
 
 const Input = ({getCatImage}: InputProps) => {
+
+  const [text, setText] = useState<string>('')
+
+  const handleButtonClick = () => {
+    getCatImage(text)
+  }
+
   return (
-    <div>
-      
+    <div className={inputContainer}>
+      <input
+        className={inputStyle}
+        type="text" value={text}
+        placeholder="고양이"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button className={inputButtonStyle} onClick={handleButtonClick}>
+          고양이만들기
+        </button>
     </div>
   )
 }
+
+const inputContainer = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const inputStyle = css`
+`
+
+const inputButtonStyle = css`
+`
 
 export default Input
